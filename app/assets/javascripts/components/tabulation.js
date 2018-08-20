9e6ec87e051016681f213a2e073dd9da484d3c36
AyarosF <sorayafructuoso@gmail.com>
2018-08-20 19:09:10 +0200

    mise en place tabs

--- app/assets/javascripts/components/tabulation.js
+++ app/assets/javascripts/components/tabulation.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  $('h1').hide();
+ $('ul.tabs li').click(function(){
+   var tab_id = $(this).attr('data-tab');
+   $('ul.tabs li').removeClass('current');
+   $('.tab-content').removeClass('current');
+   $(this).addClass('current');
+   $("#"+tab_id).addClass('current');
+ });
 });
